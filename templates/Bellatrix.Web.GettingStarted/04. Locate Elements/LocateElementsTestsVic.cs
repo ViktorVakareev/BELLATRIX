e6716cd85d4af0c8d80becb0dffc9414e0f85a7e
VIKTOR VAKAREEV <v.vakareev@gmail.com>
2021-09-20 11:06:52 +0300

    added tests

--- templates/Bellatrix.Web.GettingStarted/04. Locate Elements/LocateElementsTestsVic.cs
+++ templates/Bellatrix.Web.GettingStarted/04. Locate Elements/LocateElementsTestsVic.cs
@@ -7,12 +7,12 @@ using System.Threading.Tasks;
 
 namespace Bellatrix.Web
 {
-////1. Create a new MSTest test class
-////2. Use the BELLATRIX Browser attribute so that you execute all tests in Firefox and restart the browser every time.
-////3. Navigate to http://demos.bellatrix.solutions/welcome/ for each test but don't navigate in the tests body.
-////5. Create a test where you click on the Saturn V Sale! Button via locating the element by inner text.
-////6. Create a test where you find all Add to cart buttons
-////7. Create a test where you locate all images inside the Best Sellers section. Use chain locators.
+    ////1. Create a new MSTest test class
+    ////2. Use the BELLATRIX Browser attribute so that you execute all tests in Firefox and restart the browser every time.
+    ////3. Navigate to http://demos.bellatrix.solutions/welcome/ for each test but don't navigate in the tests body.
+    ////5. Create a test where you click on the Saturn V Sale! Button via locating the element by inner text.
+    ////6. Create a test where you find all Add to cart buttons
+    ////7. Create a test where you locate all images inside the Best Sellers section. Use chain locators.
     [TestFixture]
     [Browser(BrowserType.Firefox, Lifecycle.RestartEveryTime)]
     public class LocateElementsTestsVic : NUnit.WebTest
@@ -42,19 +42,20 @@ namespace Bellatrix.Web
 
         [Test]
         public void LocateAllImages_InsideBestSellerSection_UsingXpath()
-        {           
+        {
             var bestSellerSectionImages = App.Components.CreateAllByXpath<Anchor>("//h2[(contains(text(),'Best Sellers'))]/following::img").ToList();
-            
+
             Assert.AreEqual(5, bestSellerSectionImages.Count);
         }
 
         [Test]
         public void LocateAllImages_InsideBestSellerSection_UsingNestedLocators()
         {
-           //// var bestSellerSection = App.Components.CreateByXpath<Anchor>("//h2[(contains(text(),'Best Sellers'))]");
-           //// var bestSellerSectionImages = bestSellerSection.CreateByClassContaining<Anchor>("woocommerce columns-4 ").CreateByXpath<Anchor>("(//img)[1]");//.ToArray();
-           //// bestSellerSectionImages.Click();
-           //// Assert.AreEqual(5, bestSellerSectionImages.Count);
+            ////var bestSellerSection = App.Components.CreateByXpath<Option>("//h2[(contains(text(),'Best Sellers'))]");
+            //var bestSellerSection = App.Components.CreateByXpath<Option>("//h2[(contains(text(),'Best Sellers'))]");
+            //var bestSellerSectionImages = bestSellerSection.CreateByClassContaining<Anchor>("woocommerce columns-4 ").CreateAllByXpath<Anchor>("(//img)");//.ToArray();
+            //bestSellerSectionImages.Click();
+            //Assert.AreEqual(4, bestSellerSectionImages.Count);
         }
     }
 }
